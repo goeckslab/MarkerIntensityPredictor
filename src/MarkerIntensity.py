f0ea54af6bdceaa6c9432cd90634e30b3c26f7ce
Exitare <business.exitare@icloud.com>
2021-03-23 12:30:02 -0700

    Finished initial transition of notebook to py script.

---
+++ src/MarkerIntensity.py
@@ -0,0 +1,11 @@
+class MarkerIntensity:
+    input_file: str
+
+    def __init__(self, input_file):
+        self.input_file = input_file
+
+    def train(self):
+        pass
+
+    def predictions(self):
+        pass
