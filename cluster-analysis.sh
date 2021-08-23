96dc01d1beadc459a21f246b6735a8feadadeccd
Exitare <business.exitare@icloud.com>
2021-08-23 13:44:56 -0700

    Adjusted script.

--- cluster-analysis.sh
+++ cluster-analysis.sh
@@ -3,8 +3,8 @@
 
 source ./venv/bin/activate
 
-runs=$1
-
+folder=$1
+runs=$2
 
 
 for ((i = 1; i <= $runs; i++)); 
@@ -12,23 +12,23 @@ do
     folderName=Run$i
 
     echo Creating folder: $folderName
-
+    
     mkdir -p results/cluster_analysis/$folderName
 
 
     python3 src/main.py cl -f \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-2-1/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/PCA/9-2-1/encoded_data.csv  \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-2-1/test_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-2-2/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/PCA/9-2-2/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-2-2/test_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-3-1/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/PCA/9-3-1/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-3-1/test_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-3-2/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/PCA/9-3-2/encoded_data.csv \
-    /Users/raphael/Box/Goecks\ Precision\ Oncology\ Analytics/Projects/Raphael/Marker\ Intensity\ Predictions/Experiment\ 07-13-21/VAE/9-3-2/test_data.csv \
+    $folder/vae/9-2-1/vae_encoded_data.csv \
+    $folder/pca/9-2-1/pca_encoded_data.csv  \
+    $folder/vae/9-2-1/test_data.csv \
+    $folder/vae/9-2-2/vae_encoded_data.csv \
+    $folder/pca/9-2-2/pca_encoded_data.csv \
+    $folder/vae/9-2-2/test_data.csv \
+    $folder/vae/9-3-1/vae_encoded_data.csv \
+    $folder/pca/9-3-1/pca_encoded_data.csv \
+    $folder/vae/9-3-1/test_data.csv \
+    $folder/vae/9-3-2/vae_encoded_data.csv \
+    $folder/pca/9-3-2/pca_encoded_data.csv \
+    $folder/vae/9-3-2/test_data.csv \
     -n vae pca non
 
     for file in results/cluster_analysis/*; do 
