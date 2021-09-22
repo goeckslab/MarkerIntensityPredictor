0c0f797210693eb5bf5a63f3401ef9b11b6d841b
Exitare <business.exitare@icloud.com>
2021-09-22 09:38:03 -0700

    Updated model training step.

--- src/VAE/vae_model.py
+++ src/VAE/vae_model.py
@@ -27,20 +27,24 @@ class VAE(keras.Model):
             reconstruction = self.decoder(z)
             reconstruction_loss_fn = keras.losses.MeanSquaredError()
             reconstruction_loss = reconstruction_loss_fn(data, reconstruction)
-            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            # Set the beta value to 3.
+            kl_loss = 3 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
+            kl_loss *= -0.5
             kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
             total_loss = reconstruction_loss + kl_loss
 
-        grads = tape.gradient(reconstruction_loss, self.trainable_weights)
+        grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
-        self.total_loss_tracker.update_state(reconstruction_loss)
+        self.total_loss_tracker.update_state(total_loss)
         self.reconstruction_loss_tracker.update_state(reconstruction_loss)
-        self.kl_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
         return {
             "loss": self.total_loss_tracker.result(),
             "reconstruction_loss": self.reconstruction_loss_tracker.result(),
             "kl_loss": self.kl_loss_tracker.result(),
         }
+
+
 
     def call(self, inputs):
         z_mean, z_log_var, z = self.encoder(inputs)
