e8b03edf8b914feff2ce0f2cc88824e81304c22e
Exitare <business.exitare@icloud.com>
2022-03-04 11:32:50 -0800

    Updated run script.

--- runs.sh
+++ runs.sh
@@ -1,6 +1,15 @@
 
-run_name=$1
-file_name=$2
+experiment_name=$1
+run_name=$2
+file_name=$3
+server=$4
+
+if [ -z "$experiment_name" ]
+then
+      echo "Please specify an experiment name"
+	  exit 1
+fi
+
 
 if [ -z "$run_name" ]
 then
@@ -14,9 +23,15 @@ then
 	  exit 1
 fi
 
+if [ -z "$server" ]
+then
+      echo "Using localhost tracking server"
+	  $server="http://127.0.0.1:5000"
+fi
+
 for RUN in {1..15}
 do
 	echo "Starting run ${RUN}"
 	source venv/bin/activate
-	python3 src/marker_intensity_predictor.py -e VAE_AE_Comparison --file ${file_name} -t http://127.0.0.1:5000 -r ${run_name}_"#${RUN}"
+	python3 src/marker_intensity_predictor.py -e ${experiment_name} --file ${file_name} -t ${server} -r ${run_name}_"#${RUN}"
 done
