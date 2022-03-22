9dfe99a976ff7f918d1fe9046bd4baafbdfef393
Exitare <business.exitare@icloud.com>
2022-03-22 13:28:52 -0700

    Formatted files and added comments.

--- compare_2_files.sh
+++ compare_2_files.sh
@@ -1,3 +1,4 @@
+# Compares two specifically given files
 
 experiment_name=$1
 file_name=$2
@@ -5,46 +6,39 @@ compare_file_name=$3
 seed=$4
 server=$5
 
-if [ -z "$experiment_name" ]
-then
-      echo "Please specify an experiment name"
-	  exit 1
+if [ -z "$experiment_name" ]; then
+  echo "Please specify an experiment name"
+  exit 1
 fi
 
-if [ -z "$file_name" ]
-then
-      echo "Please specify a file name"
-	  exit 1
+if [ -z "$file_name" ]; then
+  echo "Please specify a file name"
+  exit 1
 fi
 
-if [ -z "$compare_file_name" ]
-then
-      echo "Please specify a file name to compare."
-	  exit 1
+if [ -z "$compare_file_name" ]; then
+  echo "Please specify a file name to compare."
+  exit 1
 fi
 
-if [ -z "$server" ]
-then
-    echo "Using localhost tracking server"
-	  server="http://127.0.0.1:5000"
+if [ -z "$server" ]; then
+  echo "Using localhost tracking server"
+  server="http://127.0.0.1:5000"
 fi
 
-if [ -z "$seed" ]
-then
-      echo "No seed provided. Defaulting to 1"
-      seed=1
+if [ -z "$seed" ]; then
+  echo "No seed provided. Defaulting to 1"
+  seed=1
 fi
 
- for RUN in {1..5}
-    do
-	    echo "Starting run ${RUN} comparing ${file_name} with ${compare_file_name}"
-	    source venv/bin/activate
-	    python3 multi_biopsy_marker_prediction.py -e "${experiment_name}" --file "${file_name}" "${compare_file_name}" -r "${RUN}" -s $seed
-    done
+for RUN in {1..5}; do
+  echo "Starting run ${RUN} comparing ${file_name} with ${compare_file_name}"
+  source venv/bin/activate
+  python3 multi_biopsy_marker_prediction.py -e "${experiment_name}" --file "${file_name}" "${compare_file_name}" -r "${RUN}" -s $seed
+done
 
-for RUN in {1..5}
-  do
-	  echo "Starting run ${RUN} comparing ${compare_file_name} with ${file_name}"
-	  source venv/bin/activate
-	  python3 multi_biopsy_marker_prediction.py -e "${experiment_name}" --file "${compare_file_name}" "${file_name}" -r "${RUN}" -s $seed
-  done
+for RUN in {1..5}; do
+  echo "Starting run ${RUN} comparing ${compare_file_name} with ${file_name}"
+  source venv/bin/activate
+  python3 multi_biopsy_marker_prediction.py -e "${experiment_name}" --file "${compare_file_name}" "${file_name}" -r "${RUN}" -s $seed
+done
