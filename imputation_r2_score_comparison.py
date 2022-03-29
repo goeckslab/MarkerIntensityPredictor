11a1f3de09cb2427ab33253a9fe3f87382f77c08
Exitare <business.exitare@icloud.com>
2022-03-29 09:50:29 -0700

    Added an app to compare r2 scores for imputation.

---
+++ imputation_r2_score_comparison.py
@@ -0,0 +1,108 @@
+from pathlib import Path
+import argparse
+from typing import Optional
+import mlflow
+import pandas as pd
+from library.mlflow_helper.experiment_handler import ExperimentHandler
+from mlflow.entities import Run
+from library.data.folder_management import FolderManagement
+from library.data.data_loader import DataLoader
+from library.mlflow_helper.reporter import Reporter
+from library.plotting.plots import Plotting
+from library.mlflow_helper.run_handler import RunHandler
+
+base_path = Path("imputation_score_comparison")
+
+
+def get_args():
+    """
+    Load all provided cli args
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--run", "-r", action="store", required=True,
+                        help="The name of the run being run",
+                        type=str)
+    parser.add_argument("--tracking_url", "-t", action="store", required=False,
+                        help="The tracking url for the mlflow tracking server", type=str,
+                        default="http://127.0.0.1:5000")
+    parser.add_argument("--experiment", "-e", action="store", required=False,
+                        help="Assigns the run to a particular experiment. "
+                             "If the experiment does not exists it will create a new one.",
+                        default="Default", type=str)
+    parser.add_argument("--runs", action="store", nargs='+', required=False,
+                        help="The runs which should be compared", type=str)
+
+    return parser.parse_args()
+
+
+def get_associated_experiment_id(args) -> Optional[str]:
+    # The id of the associated
+    associated_experiment_id = None
+
+    experiment_name = args.experiment
+    if experiment_name is not None:
+        associated_experiment_id = experiment_handler.get_experiment_id_by_name(experiment_name=experiment_name)
+
+    # Experiment not found
+    if associated_experiment_id is None:
+        raise ValueError(
+            f"Experiment {experiment_name} not found! Either specify a different name or set create_experiment = True.")
+
+    mlflow.set_experiment(experiment_id=associated_experiment_id)
+
+    return associated_experiment_id
+
+
+if __name__ == "__main__":
+    args = get_args()
+    FolderManagement.create_directory(base_path)
+
+    # Create mlflow tracking client
+    client = mlflow.tracking.MlflowClient(tracking_uri=args.tracking_url)
+    experiment_handler: ExperimentHandler = ExperimentHandler(client=client)
+
+    associated_experiment_id: str = get_associated_experiment_id(args)
+    try:
+        with mlflow.start_run(experiment_id=associated_experiment_id, run_name=args.run.strip()) as run:
+            mlflow.log_param("Runs used", args.runs)
+
+            runs: [] = []
+            for run_name in args.runs:
+                run_name = run_name.strip()
+                run: Run = experiment_handler.get_run_by_name(experiment_id=associated_experiment_id,
+                                                              run_name=run_name)
+                if run is None:
+                    continue
+                runs.append(run)
+
+            run_directories: [] = experiment_handler.download_artifacts(runs=runs, base_save_path=base_path,
+                                                                        mlflow_folder="")
+
+            imputed_r2_scores: pd.DataFrame = pd.DataFrame()
+            frames = []
+            r2_scores: dict = {}
+            for directory in run_directories:
+                data: pd.DataFrame = DataLoader.load_file(load_path=directory, file_name="imputed_r2_score.csv")
+
+                if data is None:
+                    continue
+
+                run_name: str = RunHandler.get_run_name_by_run_id(run_id=directory.stem, runs=runs)
+
+                if run_name is None:
+                    raise ValueError(f"Run name for id {directory.stem} is none")
+
+                r2_scores[run_name] = data
+
+            plotter: Plotting = Plotting(base_path=base_path, args=args)
+            plotter.r2_scores(r2_scores=r2_scores, file_name="Imputation Performance")
+
+
+
+
+
+    except BaseException as ex:
+        print(ex)
+    finally:
+        # FolderManagement.delete_directory(base_path)
+        pass
