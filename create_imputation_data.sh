6bea0ec7fa36d0f4802770d88da926f813965d83
Exitare <business.exitare@icloud.com>
2022-04-18 14:38:17 -0700

    Renamed imputation shell script

--- create_imputation_data.sh
+++ create_imputation_data.sh
@@ -3,9 +3,6 @@ run_name=$2
 file=$3
 percentage=$4
 
-
-
-
 python3 se_imputation_zero_replacement.py -e "${experiment}" --model "${experiment}" "${run_name}" "VAE" --file "${file}" -r "VAE Imputation" --steps 5 -p "${percentage}"
 python3 me_imputation_zero_replacement.py -e "${experiment}" --model "${experiment}" "${run_name}" "ME VAE" --file "${file}" -r "ME VAE Imputation" --steps 5 -p "${percentage}"
 python3 simple_imputation.py -e "${experiment}" -r "SI" --files "${file}" -p "$percentage"
