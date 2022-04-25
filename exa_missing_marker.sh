36eff11cb8f16347c6d6392b12ddd7fde7b96b2a
Exitare <business.exitare@icloud.com>
2022-04-25 13:59:07 -0700

    Updated exacloud script.

--- exa_missing_marker.sh
+++ exa_missing_marker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-#SBATCH --time 24:00:00
-#SBATCH -p gpu --gres gpu:v100:1
+#SBATCH --nodes=1
+#SBATCH --job-name=vae_exa_cloud.sh
+#SBATCH --time=0-24:00:00
+#SBATCH --partition=exacloud
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=kirchgae@ohsu.edu
-#SBATCH --mem 64G
 
 source venv/bin/activate
 python3 remove_marker_from_training.sh --data
