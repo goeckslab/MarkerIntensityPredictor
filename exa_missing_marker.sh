d52dca4d63c87799fbe8011a2a7852db2afdee38
Exitare <business.exitare@icloud.com>
2022-04-25 21:27:00 -0700

    Increasing cpus per task.

--- exa_missing_marker.sh
+++ exa_missing_marker.sh
@@ -5,7 +5,7 @@
 #SBATCH --time=0-24:00:00
 #SBATCH --partition=exacloud
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=100
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=END,FAIL
