8b406f9a484d3e69b19884e39ebb53755e7e9050
Exitare <business.exitare@icloud.com>
2023-03-03 19:07:31 -0800

    updated exa script.

--- mesmer/tumor_out_patient/exacloud_scheduler.sh
+++ mesmer/tumor_out_patient/exacloud_scheduler.sh
@@ -2,10 +2,25 @@
 
 test_id=$1
 
+
+
+
 markers=('pRB' 'CD45' 'CK19' 'Ki67' 'aSMA' 'Ecad' 'PR' 'CK14' 'HER2' 'AR' 'CK17' 'p21' 'Vimentin' 'pERK' 'EGFR' 'ER')
 #markers=('pRB')
 for marker in "${markers[@]}"; do
   echo "${marker}"
   echo test_id="${test_id}" marker="${marker}"
+
+  #SBATCH --nodes=1
+  #SBATCH --job-name=mesmer_out_patient
+  #SBATCH --time=0-24:00:00
+  #SBATCH --partition=exacloud
+  #SBATCH --ntasks=1
+  #SBATCH --cpus-per-task=1
+  #SBATCH --output=./output_reports/slurm.%N.%j.out
+  #SBATCH --error=./error_reports/${marker}_slurm.%N.%j.err
+  #SBATCH --mail-type=END,FAIL
+  #SBATCH --mail-user=kirchgae@ohsu.edu
+
   sbatch exacloud_executor.sh "${test_id}" "${marker}"
 done
