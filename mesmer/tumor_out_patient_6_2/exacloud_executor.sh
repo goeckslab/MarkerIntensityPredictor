990300c8c328deffb837919b7f67798e61d7fff3
Exitare <business.exitare@icloud.com>
2023-04-26 10:05:54 -0700

    Updated scripts to accomodate new 6/2 splits.

---
+++ mesmer/tumor_out_patient_6_2/exacloud_executor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=mesmer_out_patient
+#SBATCH --time=0-24:00:00
+#SBATCH --partition=exacloud
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output=./output_reports/slurm.%N.%j.out
+#SBATCH --error=./error_reports/slurm.%N.%j.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=kirchgae@ohsu.edu
+
+
+test_id=$1
+marker=$2
+
+
+
+
+make -f makefile ludwig-experiment test_id="${test_id}" marker="${marker}" &&
+  make -f makefile ludwig-evaluate test_id="${test_id}" marker="${marker}" &&
+  make -f makefile ludwig-plots test_id="${test_id}" marker="${marker}" &&
+  make -f makefile clean test_id="${test_id}" marker="${marker}"
