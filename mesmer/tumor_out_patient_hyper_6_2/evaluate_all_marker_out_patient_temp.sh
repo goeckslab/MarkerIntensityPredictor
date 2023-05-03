facac2b8a56740fc9ab88feb3b8c2cdea8068ecb
Exitare <business.exitare@icloud.com>
2023-05-03 11:36:27 -0700

    Added new scripts for hyper opt

---
+++ mesmer/tumor_out_patient_hyper_6_2/evaluate_all_marker_out_patient_temp.sh
@@ -0,0 +1,15 @@
+test_id=$1
+
+markers=('pRB' 'CD45' 'CK19' 'Ki67' 'aSMA' 'Ecad' 'PR' 'CK14' 'HER2' 'AR' 'CK17' 'p21' 'Vimentin' 'pERK' 'EGFR' 'ER')
+#markers=('pRB')
+for marker in "${markers[@]}"; do
+  patient=${test_id:0:4}
+  echo "${marker}"
+  echo test_id="${test_id}" marker="${marker}" patient_id="${patient}"
+
+  make -f makefile ludwig-hyperopt  test_id="${test_id}" marker="${marker}" patient_id="${patient}" && \
+  make -f makefile ludwig-experiment  test_id="${test_id}" marker="${marker}" patient_id="${patient}" && \
+  make -f makefile ludwig-evaluate  test_id="${test_id}" marker="${marker}" patient_id="${patient}" && \
+  make -f makefile ludwig-plots  test_id="${test_id}" marker="${marker}" patient_id="${patient}" && \
+  make -f makefile clean  test_id="${test_id}" marker="${marker}" patient_id="${patient}"
+done
