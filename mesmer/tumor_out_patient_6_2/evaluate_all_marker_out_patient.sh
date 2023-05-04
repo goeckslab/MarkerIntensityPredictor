adef55effb3eae7ff3550f7761057703e27e1303
Exitare <business.exitare@icloud.com>
2023-05-04 14:07:30 -0700

    Updated scripts for exacloud usage.

--- mesmer/tumor_out_patient_6_2/evaluate_all_marker_out_patient.sh
+++ mesmer/tumor_out_patient_6_2/evaluate_all_marker_out_patient.sh
@@ -1,10 +1,11 @@
 test_id=$1
+patient=$2
 
 markers=('pRB' 'CD45' 'CK19' 'Ki67' 'aSMA' 'Ecad' 'PR' 'CK14' 'HER2' 'AR' 'CK17' 'p21' 'Vimentin' 'pERK' 'EGFR' 'ER')
+#markers=('pERK' 'EGFR' 'ER')
+
 for marker in "${markers[@]}"; do
   echo "${marker}"
-
-  patient=${test_id:0:3}
   echo test_id="${test_id}" marker="${marker}" patient_id="${patient}"
 
   make -f makefile ludwig-experiment test_id="${test_id}" marker="${marker}" patient_id="${patient}" &&
--- mesmer/tumor_out_patient_6_2/exacloud_starter.sh
+++ mesmer/tumor_out_patient_6_2/exacloud_starter.sh
@@ -11,12 +11,12 @@
 #SBATCH --mail-user=kirchgae@ohsu.edus
 
 test_id=$1
+patient=$2
 
 markers=('pRB' 'CD45' 'CK19' 'Ki67' 'aSMA' 'Ecad' 'PR' 'CK14' 'HER2' 'AR' 'CK17' 'p21' 'Vimentin' 'pERK' 'EGFR' 'ER')
-#markers=('pRB')
 for marker in "${markers[@]}"; do
   echo "${marker}"
   echo test_id="${test_id}" marker="${marker}"
 
-  sbatch exacloud_executor.sh "${test_id}" "${marker}"
+  sbatch exacloud_executor.sh "${test_id}" "${marker}" "${patient}"
 done
