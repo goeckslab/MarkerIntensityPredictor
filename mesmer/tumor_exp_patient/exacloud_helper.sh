6d86b638e70efcf4bce01832b93873b833600e19
Exitare <business.exitare@icloud.com>
2023-05-05 13:08:17 -0700

    Updated requirements.txt

--- mesmer/tumor_exp_patient/exacloud_helper.sh
+++ mesmer/tumor_exp_patient/exacloud_helper.sh
@@ -19,4 +19,4 @@ random_seed=$4
 make -f makefile ludwig-experiment test_id="${test_id}" marker="${marker}" patient_id="${patient}" random_seed="${random_seed}" &&
   #make -f makefile ludwig-evaluate test_id="${test_id}" marker="${marker}" patient_id="${patient}" &&
   #make -f makefile ludwig-plots test_id="${test_id}" marker="${marker}" patient_id="${patient}" &&
-make -f makefile clean test_id="${test_id}" marker="${marker}" patient_id="${patient}"
+#make -f makefile clean test_id="${test_id}" marker="${marker}" patient_id="${patient}"
