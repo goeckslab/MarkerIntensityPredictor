2c5568a3f2e2e08cac4cd991ad2a51df303e0d1f
Exitare <business.exitare@icloud.com>
2023-05-06 17:39:11 -0700

    Updated exa scripts

--- mesmer/tumor_exp_patient_sp_138/exacloud_helper.sh
+++ mesmer/tumor_exp_patient_sp_138/exacloud_helper.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --job-name=mesmer_exp_patient_sp_138
-#SBATCH --time=0-72:00:00
+#SBATCH --time=0-24:00:00
 #SBATCH --partition=exacloud
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=4
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
 #SBATCH --mail-type=FAIL
