b4e8b89148ccd3cc08514aca16f5a532b6a4573e
Exitare <business.exitare@icloud.com>
2023-05-08 15:07:07 -0700

    Updated exa script

--- ae_imputation/ae.py
+++ ae_imputation/ae.py
@@ -143,9 +143,6 @@ def create_results_folder(spatial_radius: str) -> Path:
     if not save_path.exists():
         save_path.mkdir(parents=True)
 
-    print(save_path)
-    input()
-
     return save_path
 
 
