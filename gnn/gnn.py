4bbc9be7e18f81970f173a79e6d93824321aba55
Exitare <business.exitare@icloud.com>
2023-05-08 15:54:36 -0700

    First iteration of gnn

---
+++ gnn/gnn.py
@@ -0,0 +1,116 @@
+import torch
+from torch.nn import Linear
+from torch_geometric.data import Data
+from torch_geometric.nn import GCNConv
+
+from scipy.spatial.distance import cdist
+
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+import os, argparse
+from pathlib import Path
+
+
+class GCNAutoencoder(torch.nn.Module):
+    def __init__(self, num_features: int):
+        super().__init__()
+        torch.manual_seed(1234)
+
+        # Encoder layers.
+        self.conv_enc1 = GCNConv(num_features, 10)
+        self.conv_enc2 = GCNConv(10, 5)
+        self.conv_enc3 = GCNConv(5, 2)
+
+        # Decoder layers.
+        self.conv_dec1 = GCNConv(2, 5)
+        self.conv_dec2 = GCNConv(5, 10)
+        self.conv_dec3 = GCNConv(10, num_features)
+
+    def forward(self, x, edge_index):
+        # Encode.
+        x = self.conv_enc1(x, edge_index)
+        x = x.tanh()
+        x = self.conv_enc2(x, edge_index)
+        x = x.tanh()
+        x = self.conv_enc3(x, edge_index)
+        x = x.tanh()  # Final embedding.
+        embedding = x
+
+        # Decode.
+        x = self.conv_dec1(x, edge_index)
+        x = x.tanh()
+        x = self.conv_dec2(x, edge_index)
+        x = x.tanh()
+        x = self.conv_dec3(x, edge_index)
+        out = x.tanh()
+
+        return out, embedding
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--folder",
+                        help="The folder where all files are stored. Should not be a top level folder ", required=True)
+
+    args = parser.parse_args()
+
+    folder = args.folder
+    biopsy = Path(folder).stem
+    patient = "_".join(Path(folder).stem.split("_")[:2])
+    test_set = pd.read_csv(str(Path(folder, f"test_set.csv")), header=0)
+    test_edge_index = torch.load(str(Path(folder, f"test_edge_index.pt")))
+    train_set = pd.read_csv(str(Path(folder, f"train_set.csv")), header=0)
+    train_edge_index = torch.load(str(Path(folder, f"train_edge_index.pt")))
+
+    print(biopsy)
+    print(folder)
+    print(patient)
+
+    train_node_features = torch.tensor(train_set.values, dtype=torch.float)
+    num_train_cells = train_set.shape[0]
+    train_mask = np.full(num_train_cells, False)
+
+    test_node_features = torch.tensor(test_set.values, dtype=torch.float)
+    num_test_cells = test_set.shape[0]
+    test_mask = np.full(num_test_cells, True)
+
+    train_data = Data(x=train_node_features, train_mask=train_mask, edge_index=train_edge_index,
+                      y=train_set['CD45'].values)
+    test_data = Data(x=test_node_features, train_mask=test_mask, edge_index=test_edge_index,
+                     y=test_set['CD45'].values)
+
+    model = GCNAutoencoder(num_features=len(train_set.columns))  # Create new network.
+    criterion = torch.nn.L1Loss()  # Define loss criterion.
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Define optimizer.
+    out = []
+
+
+    def train(data: Data):
+        optimizer.zero_grad()  # Clear gradients.
+        out, h = model(data.x, data.edge_index)  # Perform a single forward pass.
+        loss = criterion(out, data.x)  # Compute the loss.
+        loss.backward()  # Derive gradients.
+        optimizer.step()  # Update parameters based on gradients.
+        return loss, h
+
+
+    for epoch in range(100):
+        loss, h = train(train_data)
+
+    # Evaluate model performance on test set.
+    model.eval()
+    with torch.no_grad():
+        out, h = model(test_data.x, test_data.edge_index)
+        loss = criterion(out, test_data.x)  # Compute the loss.
+    print("total loss", loss.item())
+
+    print("per marker loss:")
+    out_test = out
+    data_test = test_data.x
+    for i, col in enumerate(test_set.columns):
+        out_marker = out_test[:, i]
+        data_marker = data_test[:, i]
+        loss = criterion(out_marker, data_marker)
+        print("\t", col, loss.item())
---
+++ gnn/prepare_data.py
@@ -0,0 +1,115 @@
+import argparse, os
+from pathlib import Path
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from scipy.spatial.distance import cdist
+import torch
+
+save_path = Path("gnn/data")
+markers = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
+           'pERK', 'EGFR', 'ER']
+
+
+def create_save_folder(save_path: Path, biopsy, mode: str, spatial: str) -> Path:
+    save_path = Path(save_path, mode)
+    save_path = Path(save_path, biopsy)
+    save_path = Path(save_path, spatial)
+
+    if not save_path.exists():
+        save_path.mkdir(parents=True)
+
+    return save_path
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-b", "--biopsy", help="The test biopsy", required=True)
+    parser.add_argument("-m", "--mode", choices=["ip", "exp"], default="ip")
+    parser.add_argument("-sp", "--sp", choices=["23", "46", "92", "138", "184"],
+                        help="The distance in pixels between nodes", default="46")
+
+    args = parser.parse_args()
+
+    biopsy = args.biopsy
+    biopsy_name = Path(biopsy).stem
+    patient = "_".join(Path(args.biopsy).stem.split("_")[:2])
+    mode = args.mode
+    spatial = args.sp
+
+    print(f"Mode: {mode}")
+    print(f"Biopsy: {biopsy_name}")
+    print(f"Spatial: {spatial}")
+
+    test_set = pd.read_csv(str(Path("data/tumor_mesmer", f"{biopsy_name}.csv")), header=0)
+
+    save_path = create_save_folder(save_path, biopsy_name, mode, spatial)
+
+    if mode == "ip":
+        if biopsy_name[-1] == "1":
+            train_biopsy = biopsy_name[:-1] + "2"
+        else:
+            train_biopsy = biopsy_name[:-1] + "1"
+
+        # load train set from the folder of the biopsy
+        train_set = pd.read_csv(str(Path("data/tumor_mesmer", f"{train_biopsy}.csv")), header=0)
+
+    elif mode == "exp":
+        train_set = []
+        for root, dirs, files in os.walk(str(Path("data/tumor_mesmer"))):
+            for name in files:
+                if Path(name).suffix == ".csv" and patient not in name and "excluded" not in name:
+                    print("Loading {}".format(name))
+                    train_set.append(pd.read_csv(os.path.join(root, name), header=0))
+
+        assert len(train_set) == 6, "Expected 6 files, got {}".format(len(train_set))
+
+        train_set = pd.concat(train_set, axis=0)
+
+    else:
+        raise ValueError("Invalid mode")
+
+    train_markers_df = train_set[markers]
+
+    # Transform the data.
+    train_markers_df = np.log10(train_markers_df + 1)
+    min_max_scaler = MinMaxScaler(feature_range=(0, 1))
+    train_markers_df = pd.DataFrame(min_max_scaler.fit_transform(train_markers_df), columns=train_markers_df.columns)
+
+    test_data_df = test_set[markers]
+    test_data_df = np.log10(test_data_df + 1)
+    min_max_scaler = MinMaxScaler(feature_range=(0, 1))
+    test_data_df = pd.DataFrame(min_max_scaler.fit_transform(test_data_df), columns=test_data_df.columns)
+
+    # extract x and y coordinates as numpy array
+    train_coords = train_set[['X_centroid', 'Y_centroid']].values
+    test_coords = train_set[['X_centroid', 'Y_centroid']].values
+
+    print("Calculating pairwise distances for train set...")
+    # compute pairwise distances between nodes
+    train_distances = cdist(train_coords, train_coords)
+    print("Calculating pairwise distances for test set...")
+    test_distances = cdist(test_coords, test_coords)
+
+    print("Creating train edge index...")
+    # set distance threshold and create edge index
+    threshold = spatial
+    train_edge_index = torch.tensor(
+        [(i, j) for i in range(len(train_set)) for j in range(len(train_set)) if
+         i != j and train_distances[i, j] < threshold],
+        dtype=torch.long).t()
+
+    print("Creating test edge index...")
+    test_edge_index = torch.tensor(
+        [(i, j) for i in range(len(test_set)) for j in range(len(test_set)) if
+         i != j and test_distances[i, j] < threshold],
+        dtype=torch.long).t()
+
+    print(save_path)
+    print(biopsy_name)
+
+    # save the data
+    torch.save(train_edge_index, str(Path(str(save_path), f"train_edge_index.pt")))
+    torch.save(test_edge_index, str(Path(str(save_path), f"test_edge_index.pt")))
+    train_markers_df.to_csv(str(Path(str(save_path), f"train_set.csv")), index=False)
+    test_data_df.to_csv(str(Path(str(save_path), f"test_set.csv")), index=False)
