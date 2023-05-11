72f94932b88f73f4324bc50d8b1f345c3eb8b19a
Exitare <business.exitare@icloud.com>
2023-05-10 19:55:37 -0700

    Updated evaluation script to support ae and gnn

--- evaluate_network_performance.py
+++ evaluate_network_performance.py
@@ -6,38 +6,36 @@ import pandas as pd
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', "--path", type=str, required=True, help="path to ae_imputation folder")
+    parser.add_argument('--model', "-m", type=str, required=True, help="The model to evaluate", choices=["ae", "gnn"])
     args = parser.parse_args()
 
+    model = args.model
+    if model == "ae":
+        load_path = Path("ae_imputation")
+    elif model == "gnn":
+        load_path = Path("gnn/results")
+    else:
+        raise ValueError("Model not supported")
+
     all_scores = []
-    for root, sub_directories, files in os.walk(args.path):
+    loaded_files = 0
+    for root, sub_directories, files in os.walk(load_path):
         for sub_directory in sub_directories:
-            print(Path(root, sub_directory))
             current_path = Path(root, sub_directory)
             try:
                 if 'experiment_run' not in str(current_path):
                     continue
-
+                print(f"Loading path: {Path(root, sub_directory)}")
                 scores = pd.read_csv(Path(root, sub_directory, "scores.csv"))
-                experiment = str(current_path).split("/")[6].split("_")[-1]
-                radius = str(current_path).split("/")[5]
-                biopsy = str(current_path).split("/")[4]
-                noise = str(current_path).split("/")[3]
-                replace_value = str(current_path).split("/")[2]
-                combination = str(current_path).split("/")[1]
-
-                scores["FE"] = int(radius)
-                scores["Replace Value"] = replace_value
-                scores["Noise"] = noise
-                scores["Experiment"] = experiment
-                scores["Mode"] = "AE"
-
                 all_scores.append(scores)
+
+                loaded_files += 1
 
             except:
                 continue
 
     all_scores = pd.concat(all_scores)
+
     save_path = Path("data/scores/ae")
 
     if save_path.exists():
@@ -46,3 +44,4 @@ if __name__ == '__main__':
     save_path.mkdir(parents=True, exist_ok=True)
 
     all_scores.to_csv(Path(save_path, "scores.csv"), index=False)
+    print(f"Loaded {loaded_files} files.")
