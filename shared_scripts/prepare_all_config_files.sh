90bfa5fa9c8f7edce30aa06f75394d9f5b4bba1d
Exitare <business.exitare@icloud.com>
2023-05-11 11:30:25 -0700

    Updated config generation for spatial data

--- shared_scripts/prepare_all_config_files.sh
+++ shared_scripts/prepare_all_config_files.sh
@@ -1,8 +1,16 @@
 BASE_DIR=$1
 BIOPSY=$2
 PATIENT=$3
+SPATIAL=$4
 markers=('pRB' 'CD45' 'CK19' 'Ki67' 'aSMA' 'Ecad' 'PR' 'CK14' 'HER2' 'AR' 'CK17' 'p21' 'Vimentin' 'pERK' 'EGFR' 'ER')
 
+echo "spatial is set" ${SPATIAL}
 for marker in "${markers[@]}"; do
-    ./shared_scripts/prepare_config_files.sh $BASE_DIR $BIOPSY $marker $PATIENT
+      if [ "$SPATIAL" != "" ]; then
+          ./shared_scripts/prepare_config_files_sp.sh $BASE_DIR $BIOPSY $marker $PATIENT $SPATIAL
+      else
+         ./shared_scripts/prepare_config_files.sh $BASE_DIR $BIOPSY $marker $PATIENT
+      fi
+
+
 done
