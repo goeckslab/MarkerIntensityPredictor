133c157669571b1cb3de35d71f1001bedfad1992
Exitare <business.exitare@icloud.com>
2023-05-12 08:59:44 -0700

    Added convinience script for spatial evaluation

---
+++ schedule_all_ip_sp.sh
@@ -0,0 +1,50 @@
+cd mesmer/tumor_in_patient_sp_23
+./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2_2 9_2
+./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2_1 9_2
+./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3_2 9_3
+./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3_1 9_3
+./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14_2 9_14
+./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14_1 9_14
+./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15_2 9_15
+./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15_1 9_15
+
+cd ../tumor_in_patient_sp_46
+./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2_2 9_2
+./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2_1 9_2
+./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3_2 9_3
+./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3_1 9_3
+./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14_2 9_14
+./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14_1 9_14
+./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15_2 9_15
+./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15_1 9_15
+
+cd ../tumor_in_patient_sp_92
+./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2_2 9_2
+./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2_1 9_2
+./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3_2 9_3
+./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3_1 9_3
+./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14_2 9_14
+./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14_1 9_14
+./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15_2 9_15
+./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15_1 9_15
+
+cd ../tumor_in_patient_sp_138
+./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2_2 9_2
+./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2_1 9_2
+./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3_2 9_3
+./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3_1 9_3
+./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14_2 9_14
+./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14_1 9_14
+./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15_2 9_15
+./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15_1 9_15
+
+cd ../tumor_in_patient_sp_184
+./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2_2 9_2
+./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2_1 9_2
+./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3_2 9_3
+./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3_1 9_3
+./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14_2 9_14
+./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14_1 9_14
+./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15_2 9_15
+./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15_1 9_15
+
