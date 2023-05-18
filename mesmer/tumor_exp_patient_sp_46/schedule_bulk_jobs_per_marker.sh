28984c8016ea81da3117ed208aa8469f868d2927
Exitare <business.exitare@icloud.com>
2023-05-18 11:22:44 -0700

    Fixed exp ludwig scripts

--- mesmer/tumor_exp_patient_sp_46/schedule_bulk_jobs_per_marker.sh
+++ mesmer/tumor_exp_patient_sp_46/schedule_bulk_jobs_per_marker.sh
@@ -8,5 +8,5 @@ patient=$2
 markers=('pRB' 'CD45' 'CK19' 'Ki67' 'aSMA' 'Ecad' 'PR' 'CK14' 'HER2' 'AR' 'CK17' 'p21' 'Vimentin' 'pERK' 'EGFR' 'ER')
 for marker in "${markers[@]}"; do
     echo test_id="${test_id}" marker="${marker}" patient="${patient}"
-    sbatch exacloud_helper.sh "${test_id}" "${marker}" "${patient}"
+    sbatch marker_job.sh "${test_id}" "${marker}" "${patient}"
 done
