0989326bff6ed60b6cd9d7dbda1643fb1674dce8
Exitare <business.exitare@icloud.com>
2023-05-22 12:44:03 -0700

    Removed exp from script for faster results generation

--- schedule_all_ludwig_exp.sh
+++ schedule_all_ludwig_exp.sh
@@ -1,12 +1,12 @@
 cd mesmer/tumor_exp_patient
-./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2
-./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2
-./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3
-./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3
-./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14
-./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14
-./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15
-./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15
+#./schedule_bulk_jobs_per_marker.sh 9_2_1 9_2
+#./schedule_bulk_jobs_per_marker.sh 9_2_2 9_2
+#./schedule_bulk_jobs_per_marker.sh 9_3_1 9_3
+#./schedule_bulk_jobs_per_marker.sh 9_3_2 9_3
+#./schedule_bulk_jobs_per_marker.sh 9_14_1 9_14
+#./schedule_bulk_jobs_per_marker.sh 9_14_2 9_14
+#./schedule_bulk_jobs_per_marker.sh 9_15_1 9_15
+#./schedule_bulk_jobs_per_marker.sh 9_15_2 9_15
 
 
 cd ../tumor_exp_patient_sp_23
