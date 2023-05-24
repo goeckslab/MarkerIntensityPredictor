8ad9b61bdf408383542224ed6cb7503e2e8e546b
Exitare <business.exitare@icloud.com>
2023-05-24 09:27:28 -0700

    Fixed typo in exacloud script

--- plots/ludwig/evaluate_prediction_accurancy_in_quartiles.sh
+++ plots/ludwig/evaluate_prediction_accurancy_in_quartiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --job-name=ludwig_metrics
+#SBATCH --job-name=gbm_metrics
 #SBATCH --time=5-00:00:00
 #SBATCH --partition=exacloud
 #SBATCH --qos=long_jobs
@@ -8,7 +8,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --output=./output_reports/slurm.%N.%j.out
 #SBATCH --error=./error_reports/slurm.%N.%j.err
-#SBATCH --mail-type=FAIL, END
+#SBATCH --mail-type=FAIL,END
 #SBATCH --mail-user=kirchgae@ohsu.edu
 
 sbatch ./plots/ludwig/evaluate_prediction_accurancy_in_quartiles.sh
