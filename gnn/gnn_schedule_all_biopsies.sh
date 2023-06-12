2ef4e7194531a4e33e8a33bfd79376c10d310a83
Exitare <business.exitare@icloud.com>
2023-06-12 10:12:58 -0700

    Updated script to be able to change iterations

--- gnn/gnn_schedule_all_biopsies.sh
+++ gnn/gnn_schedule_all_biopsies.sh
@@ -5,6 +5,8 @@ iterations=$1
 biopsies=('9_2_1' '9_2_2' '9_3_1' '9_3_2' '9_14_1' '9_14_2' '9_15_1' '9_15_2')
 spatial_radius=(23 46 92 138 184)
 
+echo "iterations=${iterations}"
+
 for biopsy in "${biopsies[@]}"; do
   for radius in "${spatial_radius[@]}"; do
     for i in $(seq 1 $iterations)
