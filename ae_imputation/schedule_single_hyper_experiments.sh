a26a39468e00387939a3dde7b21b266b677f7e32
Exitare <business.exitare@icloud.com>
2023-06-13 17:30:44 -0700

    Removed echo statements

--- ae_imputation/schedule_single_hyper_experiments.sh
+++ ae_imputation/schedule_single_hyper_experiments.sh
@@ -10,7 +10,7 @@ biopsies=('9_2_1' '9_2_2' '9_3_1' '9_3_2' '9_14_1' '9_14_2' '9_15_1' '9_15_2')
 for biopsy in "${biopsies[@]}"; do
   for i in $(seq 1 $iterations)
   do
-      echo  biopsy="${biopsy}" mode="${mode}" replace_value="${replace_value}" noise="${noise}" spatial="${spatial}" hyper="${hyper}"
+      echo  biopsy="${biopsy}" mode="${mode}" replace_value="${replace_value}"
       sbatch ./ae_imputation/single_ae_hyper_experiment.sh "${biopsy}" "${mode}" "${replace_value}" "${noise}" "${spatial}"
   done
 done
