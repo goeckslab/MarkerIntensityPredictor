639a3fe76c81b6153c49aa8a594aa35e72c7be90
Exitare <business.exitare@icloud.com>
2023-07-10 10:16:20 -0700

    Added new exa cloud script

---
+++ evaluate_network_performance.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --job-name=network_metrics
+#SBATCH --time=9-00:00:00
+#SBATCH --partition=exacloud
+#SBATCH --qos=long_jobs
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --output=./output_reports/slurm.%N.%j.out
+#SBATCH --error=./error_reports/slurm.%N.%j.err
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=kirchgae@ohsu.edu
+
+network=$1
+
+python3 evaluate_network_performance.py -m $1
