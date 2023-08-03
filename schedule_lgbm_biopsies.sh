97629d6bd9e6960f5dc55e9ee691fcd522c6c91e
Exitare <business.exitare@icloud.com>
2023-08-03 10:48:56 -0700

    Renamed files

---
+++ schedule_lgbm_biopsies.sh
@@ -0,0 +1,13 @@
+sbatch ./schedule_lgbm_exp.sh 0
+sbatch ./schedule_lgbm_exp.sh 23
+sbatch ./schedule_lgbm_exp.sh 46
+sbatch ./schedule_lgbm_exp.sh 92
+sbatch ./schedule_lgbm_exp.sh 138
+sbatch ./schedule_lgbm_exp.sh 184
+
+sbatch ./schedule_lgbm_ip.sh 0
+sbatch ./schedule_lgbm_ip.sh 23
+sbatch ./schedule_lgbm_ip.sh 46
+sbatch ./schedule_lgbm_ip.sh 92
+sbatch ./schedule_lgbm_ip.sh 138
+sbatch ./schedule_lgbm_ip.sh 184
