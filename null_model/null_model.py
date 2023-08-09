f56da7173b9c77a89a14d03e8560a062fac7d4ec
Exitare <business.exitare@icloud.com>
2023-08-09 14:12:25 -0700

    Improved null model

---
+++ null_model/null_model.py
@@ -0,0 +1,83 @@
+import pandas as pd
+from ludwig.api import LudwigModel
+from pathlib import Path
+from tqdm import tqdm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+
+BIOPSIES = ["9_2_1", "9_2_2", "9_3_1", "9_3_2", "9_14_1", "9_14_2", "9_15_1", "9_15_2"]
+SHARED_MARKERS = ['pRB', 'CD45', 'CK19', 'Ki67', 'aSMA', 'Ecad', 'PR', 'CK14', 'HER2', 'AR', 'CK17', 'p21', 'Vimentin',
+                  'pERK', 'EGFR', 'ER']
+
+if __name__ == '__main__':
+
+    biopsy_evaluation = []
+
+    for i in range(101):
+        for biopsy in BIOPSIES:
+
+            patient: str = '_'.join(biopsy.split('_')[:2])
+            print(f"Processing biopsy: {biopsy} and patient {patient}")
+            # load ground truth
+            # test_data: pd.DataFrame = pd.read_csv(
+            #    Path("data", "tumor_mesmer", "preprocessed", f"{biopsy}_preprocessed_dataset.tsv"), sep="\t")
+
+            train_data: pd.DataFrame = pd.read_csv(
+                Path("data", "tumor_mesmer", "combined", "preprocessed", f"{patient}_excluded_dataset.tsv"), sep="\t")
+
+            # assert ground truth is not empty
+            assert train_data.shape[0] > 0, "Ground truth is empty"
+
+            for marker in tqdm(train_data.columns):
+                X = train_data.drop(marker, axis=1)
+                y = train_data[marker]
+
+                # split data into train and test using train test split
+                X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+
+                regression_model = LinearRegression().fit(X_train, y_train)
+
+                # draw one random samples from test data
+                x_truth = X_test.sample(n=1, random_state=42)
+                # draw same sample from y_test
+                y_truth = y_test.loc[x_truth.index]
+
+                # draw sample for ludwig with the evaluates sample index
+                evaluation_sample = train_data.loc[x_truth.index]
+
+                # assert that test sample shape is one
+                assert evaluation_sample.shape[0] == 1, "Test sample is not one"
+
+                y_hat = regression_model.predict(x_truth)
+
+                biopsy_evaluation.append({
+                    "Biopsy": biopsy,
+                    "Marker": marker,
+                    "MAE": mean_absolute_error(y_truth, y_hat),
+                    "MSE": mean_squared_error(y_truth, y_hat),
+                    "RMSE": mean_squared_error(y_truth, y_hat, squared=False),
+                    "Sample Index": evaluation_sample.index[0],
+                    "Null Model": 1,
+                    "Model Path": ""
+                })
+
+                model_path: Path = Path("mesmer", "tumor_exp_patient", biopsy, marker, "results", "experiment_run", "model")
+                print(f"Loading model from: {model_path}")
+                # load model for marker and biopsy
+                model = LudwigModel.load(str(model_path))
+                own_eval_stats, _, _ = model.evaluate(evaluation_sample)
+
+                biopsy_evaluation.append({
+                    "Biopsy": biopsy,
+                    "Marker": marker,
+                    "MAE": own_eval_stats[marker]['mean_absolute_error'],
+                    "MSE": own_eval_stats[marker]['mean_squared_error'],
+                    "RMSE": own_eval_stats[marker]['root_mean_squared_error'],
+                    "Sample Index": evaluation_sample.index[0],
+                    "Null Model": 0,
+                    "Model Path": str(model_path)
+                })
+
+    biopsy_evaluation = pd.DataFrame(biopsy_evaluation)
+    biopsy_evaluation.to_csv(Path("null_model", "sample_performance.csv"), index=False)
---
+++ null_model/visualize_performance.py
@@ -0,0 +1,97 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from pathlib import Path
+from statannotations.Annotator import Annotator
+
+
+def create_bar_chart(data: pd.DataFrame):
+    print(data)
+    input()
+    hue = "Model"
+    hue_order = ["LGBM", "Null Model"]
+    pairs = [
+        (("AR", "LGBM"), ("AR", "Null Model")),
+        (("CK14", "LGBM"), ("CK14", "Null Model")),
+        (("CK17", "LGBM"), ("CK17", "Null Model")),
+        (("CK19", "LGBM"), ("CK19", "Null Model")),
+        (("CD45", "LGBM"), ("CD45", "Null Model")),
+        (("Ecad", "LGBM"), ("Ecad", "Null Model")),
+        (("EGFR", "LGBM"), ("EGFR", "Null Model")),
+        (("ER", "LGBM"), ("ER", "Null Model")),
+        (("HER2", "LGBM"), ("HER2", "Null Model")),
+        (("Ki67", "LGBM"), ("Ki67", "Null Model")),
+        (("PR", "LGBM"), ("PR", "Null Model")),
+        (("Vimentin", "LGBM"), ("Vimentin", "Null Model")),
+        (("aSMA", "LGBM"), ("aSMA", "Null Model")),
+        (("p21", "LGBM"), ("p21", "Null Model")),
+        (("pERK", "LGBM"), ("pERK", "Null Model")),
+        (("pRB", "LGBM"), ("pRB", "Null Model")),
+
+    ]
+
+    fig, ax = plt.subplots(figsize=(15, 10))
+    sns.barplot(x="Marker", y="MAE", data=mean_df, ax=ax, hue=hue)
+
+    order = ["AR", "CK14", "CK17", "CK19", "CD45", "Ecad", "EGFR", "ER", "HER2", "Ki67", "PR", "Vimentin", "aSMA",
+             "p21", "pERK", "pRB"]
+    annotator = Annotator(ax, pairs, data=data, x="Marker", y="MAE", order=order, hue=hue, hue_order=hue_order,
+                          verbose=1)
+    annotator.configure(test='Mann-Whitney', text_format='star', loc='outside')
+    annotator.apply_and_annotate()
+
+    # plot bar plot for mean_df
+    sns.set_theme(style="whitegrid")
+    sns.set_context("paper")
+    sns.set(font_scale=1.5)
+
+    plt.show()
+
+
+if __name__ == '__main__':
+    df = pd.read_csv(Path("null_model", "sample_performance.csv"))
+
+    # create new df and pair mae values of null model and biopsy model in two columns
+    new_df = df[["Biopsy", "Marker", "MAE", "Null Model"]]
+    # create new df that puts null model and biopsy model mae values in two columns
+    new_df = new_df.pivot(index=["Biopsy", "Marker"], columns="Null Model", values="MAE")
+    # create new columns using the biopsy
+    new_df["Biopsy"] = new_df.index.get_level_values(0)
+    # rename to 0 biopsy model and 1 to null model
+    new_df.rename(columns={0: "Biopsy Model MAE", 1: "Null Model MAE"}, inplace=True)
+    # convert index marker to column marker
+    new_df.reset_index(level="Marker", inplace=True)
+    # drop index
+    new_df.reset_index(drop=True, inplace=True)
+
+    print(new_df[new_df["Biopsy"] == "9_2_1"])
+
+    # calculate mean performance for markers for both biopsy model and mull model
+    mean_df = new_df.groupby("Marker").mean()
+
+    # reset indx
+    mean_df.reset_index(inplace=True)
+
+    # Combine columns Biopsy Model MAE and NUll Model MAE to be one column
+    mean_df = mean_df.melt(id_vars=["Marker"], value_vars=["Biopsy Model MAE", "Null Model MAE"])
+    # rename value column to MAE
+    mean_df.rename(columns={"value": "MAE"}, inplace=True)
+    # rename Null Model to Model
+    mean_df.rename(columns={"Null Model": "Model"}, inplace=True)
+    # rename Biopsy Model MAE to LGBM
+    mean_df["Model"].replace({"Biopsy Model MAE": "LGBM"}, inplace=True)
+    # rename Null Model MAE to Null Model
+    mean_df["Model"].replace({"Null Model MAE": "Null Model"}, inplace=True)
+
+    create_bar_chart(data=mean_df)
+
+    # plot scatterplot
+    sns.set_theme(style="whitegrid")
+    sns.set_context("paper")
+    sns.set(font_scale=1.5)
+    fig, ax = plt.subplots(figsize=(15, 10))
+    # change x axis label to null model
+    ax.set_xlabel("Null Model")
+    ax.set_ylabel("Biopsy Model")
+    sns.scatterplot(x="Null Model MAE", y="Biopsy Model MAE", data=new_df, ax=ax, hue="Biopsy")
+    plt.show()
