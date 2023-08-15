60a628ee1addfc83034e2021b0ba073622449ad0
Exitare <business.exitare@icloud.com>
2023-08-15 10:16:10 -0700

    Updated unit test

--- artificial_data/create_artificial_data.py
+++ artificial_data/create_artificial_data.py
@@ -51,54 +51,88 @@ def gen_totally_random(n: int, columns):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", type=str, required=True, choices=["EN", "LGBM"])
+    args = parser.parse_args()
+
+    selected_model: str = args.model
 
     correlations_to_test = [None, 0, 0.5, 0.9]
     errors = []
     all_predictions = []
+    for i in tqdm(range(30)):
+        for correlation in correlations_to_test:
+            if correlation is not None:
+                # generate random data with same amount of cells like the ground truth
+                v = gen_random(n=3000, d=len(SHARED_MARKERS), covar=correlation)
+                df = pd.DataFrame(v, columns=SHARED_MARKERS)
+            else:
+                # Number of data points
+                # Generate random numbers between -1 and 1
+                df = gen_totally_random(3000, SHARED_MARKERS)
 
-    for correlation in correlations_to_test:
-        if correlation is not None:
-            # generate random data with same amount of cells like the ground truth
-            v = gen_random(n=3000, d=len(SHARED_MARKERS), covar=correlation)
-            df = pd.DataFrame(v, columns=SHARED_MARKERS)
-        else:
-            # Number of data points
-            # Generate random numbers between -1 and 1
-            df = gen_totally_random(3000, SHARED_MARKERS)
+                # scale data using minmax scaler
+            scaler = StandardScaler()
+            df = pd.DataFrame(scaler.fit_transform(df), columns=SHARED_MARKERS)
+            train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
 
-            # scale data using minmax scaler
-        scaler = StandardScaler()
-        df = pd.DataFrame(scaler.fit_transform(df), columns=SHARED_MARKERS)
-        train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+            if selected_model == "EN":
+                for marker in SHARED_MARKERS:
+                    # split train and test_df
+                    X_train = train_df.drop(marker, axis=1)
+                    y_train = train_df[marker]
 
-        for marker in SHARED_MARKERS:
-            inputs = [{"name": c, "type": "numerical"} for c in SHARED_MARKERS if c != marker]
-            outputs = [{"name": marker, "type": "numerical"}]
-            config = {
-                "input_features": inputs,
-                "output_features": outputs
-            }
+                    X_test = test_df.drop(marker, axis=1)
+                    y_test = test_df[marker]
 
-            model = LudwigModel(config)
+                    # fit elastic net model
+                    model = ElasticNetCV(cv=5, random_state=0)
+                    model.fit(X_train, y_train)
 
-            train_stats, _, model_dir = model.train(train_df)
+                    # predict on test data
+                    predictions = model.predict(X_test)
 
-            # predict on data without the marker we want to predict
+                    # save predictions
+                    errors.append({
+                        "Marker": marker,
+                        "MAE": mean_absolute_error(y_test, predictions),
+                        "RMSE": mean_squared_error(y_test, predictions),
+                        "Correlation": -1 if correlation is None else correlation,
+                        "Experiment": i
+                    })
 
-            predictions = model.predict(dataset=test_df)
-            predictions = predictions[0]
-            # rename _predictions column with marker name
-            predictions = predictions.rename(columns={f"{marker}_predictions": marker})
+            else:
+                for marker in SHARED_MARKERS:
+                    inputs = [{"name": c, "type": "numerical"} for c in SHARED_MARKERS if c != marker]
+                    outputs = [{"name": marker, "type": "numerical"}]
+                    config = {
+                        "input_features": inputs,
+                        "output_features": outputs
+                    }
 
-            # save predictions
-            errors.append({
-                "Marker": marker,
-                "MAE": mean_absolute_error(test_df[marker], predictions[marker]),
-                "RMSE": mean_squared_error(test_df[marker], predictions[marker], squared=False),
-                "Correlation": -1 if correlation is None else correlation,
-            })
+                    model = LudwigModel(config)
+
+                    train_stats, _, model_dir = model.train(train_df)
+
+                    # predict on data without the marker we want to predict
+
+                    predictions = model.predict(dataset=test_df)
+                    predictions = predictions[0]
+                    # rename _predictions column with marker name
+                    predictions = predictions.rename(columns={f"{marker}_predictions": marker})
+
+                    # save predictions
+                    errors.append({
+                        "Marker": marker,
+                        "MAE": mean_absolute_error(test_df[marker], predictions[marker]),
+                        "RMSE": mean_squared_error(test_df[marker], predictions[marker], squared=False),
+                        "Correlation": -1 if correlation is None else correlation,
+                        "Experiment": i
+                    })
 
         # save predictions
     errors = pd.DataFrame(errors)
-    errors.to_csv(Path("errors.csv"), index=False)
-
+    if selected_model == "EN":
+        errors.to_csv(Path("en_errors.csv"), index=False)
+    else:
+        errors.to_csv(Path("lgbm_errors.csv"), index=False)
