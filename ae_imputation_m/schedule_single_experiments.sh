d1086ee4fdc8c2a291a06132836ab566fd91a71e
Exitare <business.exitare@icloud.com>
2023-08-18 10:51:29 -0700

    Added exacloud sbatch.

--- ae_imputation_m/schedule_single_experiments.sh
+++ ae_imputation_m/schedule_single_experiments.sh
@@ -36,6 +36,6 @@ for biopsy in "${biopsies[@]}"; do
   for i in $(seq 1 $experiments)
   do
       echo biopsy="${biopsy}" mode="${mode}" replace_value="${replace_value}" subsets="${subsets}" spatial="${spatial}"
-       ./ae_imputation_m/single_ae_experiment.sh "${biopsy}" "${mode}" "${replace_value}" "${spatial}" "${subsets}"
+      sbatch ./ae_imputation_m/single_ae_experiment.sh "${biopsy}" "${mode}" "${replace_value}" "${spatial}" "${subsets}"
   done
 done
